425fd800889fc125ebcb2a5b41fe51444af5e5da
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-05 14:10:34 +0000

    add the about model into the admin.py to view and create instances in django admin

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from.models import AboutPage
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(AboutPage)
+class AboutPageAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'updated_on')
+    search_fields = ('title', 'mission', 'history')
+    list_filter = ('updated_on',)
+    # These will be the fields in the form that is created with summernote.
+    fields = ('title', 'mission', 'history', 'why_shop_here')
+    readonly_fields = ('updated_on',)
+    # Using Summernote for the entry.
+    summernote_fields = ('mission', 'history', 'why_shop_here')
