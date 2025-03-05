88410026a3c3957fcb43d58d0659aea96990cf67
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-05 15:57:51 +0000

    create views and templates for about app.

--- about/views.py
+++ about/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from.models import AboutPage
 
 # Create your views here.
+
+
+def about(request):
+    """
+    This will create a view that I can then use to showcase this backend data to the frontend using a template..
+    """
+    about = AboutPage.objects.first()
+    if not about:
+        about = None
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
--- observastore/urls.py
+++ observastore/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
