f18b96ee0687e3269e1751b998ca6eaa086c92b3
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-05 14:24:40 +0000

    unittest: testing the updated_on field to ensure it behaves as expected

--- about/tests.py
+++ about/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from.models import AboutPage
+from django.utils import timezone
+from time import sleep
 
 # Create your tests here.
 
@@ -10,12 +12,27 @@ class AboutPageModelTest(TestCase):
         self.about_page = AboutPage.objects.create(
             title="About Us",
             mission="Our mission is to inspire curiosity and discovery of the cosmos.",
-            history="Founded in 2010, we have been providing high-quality astronomy equipment.",
-            why_shop_here="We offer expert advice, high-quality products, and excellent customer service."
+            history="Founded in 2025, we have been providing high-quality astronomy equipment.",
+            why_shop_here="Shop with us for the best prices and quality.",
+            updated_on=timezone.now()
         )
     
     def test_about_page_works(self):
         self.assertEqual(self.about_page.title, "About Us")
-        self.assertEqual(self.about_page.mission, "This will fail")
+        self.assertEqual(self.about_page.mission, "Our mission is to inspire curiosity and discovery of the cosmos.")
+        self.assertEqual(self.about_page.history, "Founded in 2025, we have been providing high-quality astronomy equipment.")
+        self.assertEqual(self.about_page.why_shop_here, "Shop with us for the best prices and quality.")
+
+    def test_about_page_updated_on(self):
+        """Test if the 'updated_on' field is being updated automatically"""
+        # Get the current timestamp without microseconds
+        initial_updated_on = self.about_page.updated_on.replace(microsecond=0)
+        
+        # importing sleep from time to test the updated on because it happens automatically at an instant so sleep(1) will allow me to check it
+        sleep(1)  # Sleep for 1 second therefore I can ensure that there is a time difference to test it works
+        self.about_page.save()
+        updated_on = self.about_page.updated_on.replace(microsecond=0)
+        self.assertGreater(updated_on, initial_updated_on)
+
 
 
