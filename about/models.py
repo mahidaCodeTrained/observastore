3d055c92e4d69f42c91a79f40419ecdf3b08261b
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-05 14:30:14 +0000

    ran migrations and create the AboutPage model

--- about/models.py
+++ about/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AboutPage(models.Model):
+    title = models.CharField(max_length=255, default="About Us")
+    mission = models.TextField()
+    history = models.TextField()
+    why_shop_here = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
