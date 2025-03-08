42427175cb4ace9b5336f11f85397d240a23e62c
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-08 13:04:16 +0000

    unittest: create failing tests to check behaviour

--- storefront/tests.py
+++ storefront/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import StoreGoods, Category
 
 # Create your tests here.
+
+
+class StoreGoodsTesting(TestCase):
+    """ I am testing the StoreGoods model to make sure that the model behaves as intended."""
+
+    def setUp(self):
+        # Creating setUp for easy use of unittesting to avoid repeating code.
+        self.category = Category.objects.create(name="Telescopes")
+
+        # Creating a sample of the model to test.
+        self.product = StoreGoods.objects.create(
+            category=self.category,
+            sku="12345",
+            name="Telescope",
+            description="A high-quality telescope for stargazing.",
+            price="199.99",
+            weight=None,
+            image=None
+        )
+    
+    def test_storegoods_created(self):
+        self.assertEqual(self.product.name, " not Telescope")
+        self.assertEqual(self.product.sku, "123456")
+        self.assertEqual(self.product.price, "199.999")
+        self.assertEqual(self.product.category.name, "Telescopess")
+
+
