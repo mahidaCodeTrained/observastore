b8cd3469b1203ed07e2ff9560156c604d93303c7
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-08 13:27:13 +0000

    feat: update admin.py for storefront app to show models in django admin

--- storefront/admin.py
+++ storefront/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, StoreGoods
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'visual_name')
+    search_fields = ('name', 'visual_name')
+
+@admin.register(StoreGoods)
+class StoreGoodsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'sku', 'weight')
+    list_filter = ('category', 'price')
+    search_fields = ('name', 'description', 'sku')
