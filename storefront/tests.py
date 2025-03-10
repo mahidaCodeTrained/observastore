821ec09d3716b647823c42f3670c02b657a660f3
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-10 17:34:15 +0000

    unittest: create tests for viewing store products with category filter

--- storefront/tests.py
+++ storefront/tests.py
@@ -1,46 +1,51 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 from .models import StoreGoods, Category
-from django.contrib.auth import get_user_model
 
 
-class ProductDetailViewTestCase(TestCase):
+class StoreGoodsViewTests(TestCase):
     def setUp(self):
-        # Set up test data
-        self.product = StoreGoods.objects.create(
-            name='Test Product',
-            description='A test product for testing.',
-            price=99.99,
-        )
-        
-    def test_product_detail_view(self):
-        """Test that the product details page renders correctly."""
-        
-        url = reverse('product_details', kwargs={'product_id': self.product.id})  # Change this line
-        
-        response = self.client.get(url)
-        
-        # Check the response status code
-        self.assertEqual(response.status_code, 200)
-        
-        #  Im Checking that the correct template is used
-        self.assertTemplateUsed(response, 'storefront/product_details.html')
-        
-        # Check that the product data is in the context
-        self.assertEqual(response.context['product'], self.product)
-        
-        # Check that the product's name and description appear in the HTML
-        self.assertContains(response, self.product.name)
-        self.assertContains(response, self.product.description)
-        self.assertContains(response, str(self.product.price))
+        self.category_telescopes = Category.objects.create(name='Telescopes')
+        self.category_globes = Category.objects.create(name='Globes')
 
-    def test_product_not_found(self):
-        """Test that trying to view a product that doesn't exist returns a 404."""
-        
-        url = reverse('product_details', kwargs={'product_id': 9999})
-        response = self.client.get(url)
-        
-        self.assertEqual(response.status_code, 404)
+        self.product1 = StoreGoods.objects.create(
+            name='Telescope A',
+            description='A great telescope.',
+            price=100.00,
+            category=self.category_telescopes
+        )
+        self.product2 = StoreGoods.objects.create(
+            name='Globe A',
+            description='A beautiful globe.',
+            price=50.00,
+            category=self.category_globes
+        )
+
+        self.product3 = StoreGoods.objects.create(
+            name='Telescope B',
+            description='Another great telescope.',
+            price=120.00,
+            category=self.category_telescopes
+        )
+
+    def test_view_store_goods(self):
+
+        response = self.client.get(reverse('storefront'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Our Products')
+        self.assertContains(response, self.product1.name)
+        self.assertContains(response, self.product2.name)
+
+    def test_view_store_goods_with_category_filter(self):
+
+        response = self.client.get(reverse('storefront') + '?category=Telescopes')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.product1.name)
+        self.assertContains(response, self.product3.name)
+        self.assertNotContains(response, self.product2.name)
+
+
 
 
 
