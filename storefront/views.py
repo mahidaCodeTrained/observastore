de135a17a1d4c204c34d58bd90a4f22511c0f6be
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-10 17:37:07 +0000

    feat: create message functionality to allow users to know what category they are viewing the products on

--- storefront/views.py
+++ storefront/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import StoreGoods
+from .models import StoreGoods, Category
 
 # Create your views here.
-
 
 def view_store_goods(request):
     """This view is designed to show the 
@@ -13,20 +12,29 @@ def view_store_goods(request):
     # Get all products
     products = StoreGoods.objects.all()
     query = None
+    categories = None
+    category_name = None  # New variable to store category name for display
 
     if request.GET:
+        if 'category' in request.GET:
+            categories_param = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories_param)
+            categories = Category.objects.filter(name__in=categories_param)
+            category_name = ', '.join([cat.name for cat in categories])  # Format category names for display
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('storefront'))                    
-            queries = Q(name__icontains=query) | Q(
-                description__icontains=query)
+                return redirect(reverse('storefront'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = StoreGoods.objects.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,  # Will be None if no category filter is applied
+        'category_name': category_name,  # Pass the category name to template
     }
 
     return render(request, 'storefront/storefront.html', context)
