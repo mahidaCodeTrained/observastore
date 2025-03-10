0f659ce352e8fc093805ce1e11aa5d10b86a800b
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-10 09:54:06 +0000

    feat: create search functionality to search via query on the site

--- storefront/views.py
+++ storefront/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import StoreGoods
 
 # Create your views here.
@@ -10,9 +12,21 @@ def view_store_goods(request):
 
     # Get all products
     products = StoreGoods.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('storefront'))                    
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = StoreGoods.objects.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'storefront/storefront.html', context)
