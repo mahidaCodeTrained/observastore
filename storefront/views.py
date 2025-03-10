ce61b8601b115d3ba9f86d9d031b33d46865dacf
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-10 18:58:43 +0000

    feat: create filtering options through categories and price/weight

--- storefront/views.py
+++ storefront/views.py
@@ -3,25 +3,26 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import StoreGoods, Category
 
-# Create your views here.
 
 def view_store_goods(request):
-    """This view is designed to show the 
-    products that Observastore will sell."""
+    """This view is designed to show the products that Observastore will sell."""
 
     # Get all products
     products = StoreGoods.objects.all()
     query = None
     categories = None
     category_name = None  # New variable to store category name for display
+    sort_by = request.GET.get('sort', '')  # Get the sorting parameter from the request
 
     if request.GET:
+        # Apply category filter
         if 'category' in request.GET:
             categories_param = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories_param)
             categories = Category.objects.filter(name__in=categories_param)
             category_name = ', '.join([cat.name for cat in categories])  # Format category names for display
 
+        # Apply search filter
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -29,6 +30,16 @@ def view_store_goods(request):
                 return redirect(reverse('storefront'))
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = StoreGoods.objects.filter(queries)
+
+        # Apply sorting
+        if sort_by == 'price_asc':
+            products = products.order_by('price')  # Sort by price low to high
+        elif sort_by == 'price_desc':
+            products = products.order_by('-price')  # Sort by price high to low
+        elif sort_by == 'weight_asc':
+            products = products.order_by('weight')  # Sort by weight low to high
+        elif sort_by == 'weight_desc':
+            products = products.order_by('-weight')  # Sort by weight high to low
 
     context = {
         'products': products,
