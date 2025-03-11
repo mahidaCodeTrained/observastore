e330f1d77f641865b267303e24f58303620754a3
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-11 14:06:21 +0000

    feat: implement category filtering and general product filtering and product count

--- storefront/views.py
+++ storefront/views.py
@@ -11,18 +11,18 @@ def view_store_goods(request):
     products = StoreGoods.objects.all()
     query = None
     categories = None
-    category_name = None  # New variable to store category name for display
-    sort_by = request.GET.get('sort', '')  # Get the sorting parameter from the request
+    category_name = None
+    sort_by = request.GET.get('sort', '')
 
     if request.GET:
-        # Apply category filter
+
         if 'category' in request.GET:
             categories_param = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories_param)
             categories = Category.objects.filter(name__in=categories_param)
-            category_name = ', '.join([cat.name for cat in categories])  # Format category names for display
+            category_name = ', '.join([cat.name for cat in categories])
 
-        # Apply search filter
+        # Apply search filter if present
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -31,21 +31,25 @@ def view_store_goods(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = StoreGoods.objects.filter(queries)
 
-        # Apply sorting
         if sort_by == 'price_asc':
-            products = products.order_by('price')  # Sort by price low to high
+            products = products.order_by('price')
         elif sort_by == 'price_desc':
-            products = products.order_by('-price')  # Sort by price high to low
+            products = products.order_by('-price') 
         elif sort_by == 'weight_asc':
-            products = products.order_by('weight')  # Sort by weight low to high
+            products = products.order_by('weight') 
         elif sort_by == 'weight_desc':
-            products = products.order_by('-weight')  # Sort by weight high to low
+            products = products.order_by('-weight')
+        elif sort_by == 'name_asc':
+            products = products.order_by('name')
+        elif sort_by == 'name_desc':
+            products = products.order_by('-name')
 
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,  # Will be None if no category filter is applied
-        'category_name': category_name,  # Pass the category name to template
+        'current_categories': categories,
+        'category_name': category_name,
+        'sort_by': sort_by,
     }
 
     return render(request, 'storefront/storefront.html', context)
