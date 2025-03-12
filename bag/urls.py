d999379bd70d5145e1567792798e4428e80b6962
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-12 14:36:36 +0000

    Add new url path for adjusting items

--- bag/urls.py
+++ bag/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add_to_bag/<int:item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<int:item_id>/', views.adjust_bag, name='adjust_bag'),
     path('', views.view_shopping_bag, name='shopping_bag'),
 
 ]
