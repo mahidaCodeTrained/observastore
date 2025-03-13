241e2c3a3cb88b6cc891da703ecebb4f09616b83
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-13 18:38:52 +0000

    add script for stripe js

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51R2Fg6C0DsChuQrL64oyDgZcxzrFDpIb3yUZlOPfkyFKYVJPy7WG1vSvnzkEpMtpfEX7RaoST4ZiV2m4ZKatiEud00pqOpAqnL',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
