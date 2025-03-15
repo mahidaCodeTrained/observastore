be9eb7b02c67259ec00ea03a9477efe0bc573859
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-15 14:41:17 +0000

    feat: update profile app to now save information that the user inputs into their profile

--- checkout/models.py
+++ checkout/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
     user_profile = models.ForeignKey(
         UserProfile,
         on_delete=models.SET_NULL,
-        null=True, blank=True, related_name="Orders")
+        null=True, blank=True, related_name="orders")
 
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
