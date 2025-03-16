a0a022f89c71e552282eb9fb019e416e49e961c0
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-16 12:08:25 +0000

    path url for add product

--- storefront/urls.py
+++ storefront/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.view_store_goods, name='storefront'),
-    path('product/<int:product_id>/', views.detailed_products, name='product_details'),
+    path('product/<int:product_id>/',
+         views.detailed_products, name='product_details'),
+    path('add/', views.add_product, name='add_product'),
 
 ]
