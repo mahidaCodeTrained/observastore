8ad2bde71a38dd789b01cd6299a0c301cfd0d28d
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-16 14:48:38 +0000

    add widgets from django for better ux

--- storefront/forms.py
+++ storefront/forms.py
@@ -1,11 +1,16 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import StoreGoods, Category
+
 
 class StoreForm(forms.ModelForm):
 
     class Meta:
         model = StoreGoods
         fields = '__all__'
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
