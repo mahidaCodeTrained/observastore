32e8f4ade92af857937d476373ad5a440928db5d
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-17 13:22:48 +0000

    Add fix to webhooks not being accessed

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -16,38 +16,31 @@ def webhook(request):
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    # get the webhook data and verify its signature
+    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, settings.STRIPE_WH_SECRET
-        )
+        # Pass the `wh_secret` variable to the method, no need to pass it twice
+        event = stripe.Webhook.construct_event(payload, sig_header, wh_secret)
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
-
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
         return HttpResponse(status=400)
-
     except Exception as e:
-        return HttpResponse(content=e, status=400)
+        return HttpResponse(content=str(e), status=400)
 
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
     # Map webhook events to relevant handler functions
     event_map = {
-        'payment_intent.succeeded': (
-            handler.handle_payment_intent_succeeded
-            ),
-        'payment_intent.payment_failed': (
-            handler.handle_payment_intent_payment_failed
-            ),
-            }
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
 
     # Get the webhook type from Stripe
     event_type = event['type']
