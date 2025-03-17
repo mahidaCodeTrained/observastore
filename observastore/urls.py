83c63e3239d64e7a277cacb7559580f8f04b6461
mahidaCodeTrained <abhishekmahida56@gmail.com>
2025-03-17 14:15:09 +0000

    Fix: 404 error page not showing testing

--- observastore/urls.py
+++ observastore/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -35,4 +34,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-handler404 = 'boutique_ado.views.handler404'
+
+
+handler404 = 'observastore.views.handler404'
